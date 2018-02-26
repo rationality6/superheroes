d9b4e661428e77205350481c4dfea6695c357165
rationality6 <codepawn@icloud.com>
2018-02-26 21:52:39 +0900

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'app 예압 쪼아! Heroes';
 }
