c1093a684ffe6bbf87362f6fa0b699473a5fdc7c
Vincent H Ahn <codepawn@icloud.com>
2018-03-10 21:47:45 +0900

    Added rails back and .gitignore setting for rails

---
+++ client/src/app/in-memory-data.service.ts
@@ -0,0 +1,41 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Todo } from './class/todo'
+import { Hero } from './class/hero'
+
+export class InMemoryDataService implements InMemoryDbService {
+
+  createDb() {
+    const heroes: Hero[] = [
+      { id: 11, name: 'Mr. Nice' },
+      { id: 12, name: 'Narco' },
+      { id: 13, name: 'Bombasto' },
+      { id: 14, name: 'Celeritas' },
+      { id: 15, name: 'Magneta' },
+      { id: 16, name: 'RubberMan' },
+      { id: 17, name: 'Dynama' },
+      { id: 18, name: 'Dr IQ' },
+      { id: 19, name: 'Magma' },
+      { id: 20, name: 'Tornado' },
+    ]
+
+    const todos: Todo[] = [
+      { id: 0, name: "Eating rice" },
+      { id: 1, name: "Take a shit" },
+      { id: 2, name: "Thinking about dinner" },
+      { id: 3, name: "Running" },
+      { id: 4, name: "Exercise" },
+      { id: 5, name: "Coding" },
+      { id: 6, name: "Thinking about lunch" },
+      { id: 7, name: "Study" },
+      { id: 8, name: "Eating rice" },
+      { id: 9, name: "Take a shit" },
+      { id: 10, name: "Thinking about dinner" },
+      { id: 11, name: "Running" },
+      { id: 12, name: "Exercise" },
+      { id: 13, name: "Coding" },
+      { id: 14, name: "Thinking about lunch" },
+      { id: 15, name: "Study" }
+    ]
+    return { heroes, todos };
+  }
+}
